153efd0cd7b414c5ab701a7a5235e6413ff2acdb
Scott Tang <scott.q.tang@gmail.com>
2018-06-28 23:10:13 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,5 +1,5 @@
-def current_age_for_birth_year(year_of_birth)
-  return age_of_person = 2018 - year_of_birth
+def current_age_for_birth_year(birth_year)
+  return age_of_person = 2018 - birth_year
 end
 
 age_of_person = current_age_for_birth_year(1999)
